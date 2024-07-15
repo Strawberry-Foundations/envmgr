9dc5fed04a824daca9cda2349b4ff9ff66102c9f
matteodev8 <me@matteodev.xyz>
2024-07-16 00:19:44 +0200

    feat: functional chrooting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
+use std::process::{Command, Stdio};
+
 mod prep;
 
 fn main() {
-    println!("Hello, world!");
+    let chroot_dir = "/home/matteo/Junk/chroot";
+    let shell = "bash";
+
+    karen::escalate_if_needed().expect("karen fail");
+
+    prep::prep_fs(chroot_dir).expect("prep fail");
+    prep::chroot(chroot_dir).expect("chroot fail");
+
+    let mut cmd = Command::new(shell);
+    cmd.current_dir("/");
+    cmd.stdin(Stdio::inherit())
+       .stdout(Stdio::inherit())
+       .stderr(Stdio::inherit());
+
+    let mut child = cmd.spawn().expect("spawn fail");
+
+    let status = child.wait().expect("wait fail");
+
+    if !status.success() {
+        eprintln!("Shell ended with status: {}", status);
+    }
 }
--- src/prep.rs
+++ src/prep.rs
@@ -1,17 +1,19 @@
 use std::path::Path;
 use std::os::unix::fs;
 use std::env::set_current_dir;
-use nix::mount::{mount, MsFlags};
+use nix::{mount::mount, NixPath};
 
-/// Mounts required filesystems for chrooting, returns a bool that indicates if the mount process was successful
-pub fn prep_fs(chroot_path: &str) -> bool {
-    let path = Path::new(chroot_path);
+/// Mounts required filesystems for chrooting, returns a nix::Result<> that indicates if the mount process was successful
+pub fn prep_fs(chroot_path: &str) -> nix::Result<()> {    
+    if !Path::new(chroot_path).exists() {
+        return Err(nix::errno::Errno::ENOENT);
+    }
 
-    if !path.exists() { return false; }
+    bind_mnt("/dev", &format!("{chroot_path}/dev"))?;
+    bind_mnt("/sys", &format!("{chroot_path}/sys"))?;
+    bind_mnt("/proc", &format!("{chroot_path}/proc"))?;
 
-
-
-    true
+    Ok(())
 }
 
 /// chroots into a directory, it is recommended to mount the /dev, /sys and /proc filesystems beforehand
@@ -19,4 +21,10 @@ pub fn chroot(new_root: &str) -> std::io::Result<()> {
     fs::chroot(new_root).expect("Unable to chroot");
     set_current_dir("/").expect("Unable to change current directory");
     Ok(())
+}
+
+fn bind_mnt<P: ?Sized + NixPath>(source: &P, target: &P) -> nix::Result<()> {
+    let flags = nix::mount::MsFlags::MS_BIND | nix::mount::MsFlags::MS_REC;
+
+    mount(Some(source), target, None::<&str>, flags, None::<&str>)
 }
